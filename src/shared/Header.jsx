fa7ed1b3c53590db0f3a4869da0bfa75226abff5
yusufalp <alpyusuf007@gmail.com>
2024-09-10 15:46:17 -0700

    add a simple header and style it

---
+++ src/shared/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Login</a>
+          </li>
+          <li>
+            <a href="">Signup</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
