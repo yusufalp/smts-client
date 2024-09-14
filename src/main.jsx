d6c396e468f4962b7b604f435be9076d29008a32
yusufalp <alpyusuf007@gmail.com>
2024-09-13 19:41:47 -0700

    install redux and redux-persist, implement user login

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { persistor, store } from "./store/store.js";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -8,7 +12,11 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
