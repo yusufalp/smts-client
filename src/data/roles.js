ed7e8e5c643d547fc6ebe99d3709ff84005b958d
yusufalp <alpyusuf007@gmail.com>
2024-10-27 20:42:01 -0700

    create constant values for roles and statuses

---
+++ src/data/roles.js
@@ -0,0 +1,8 @@
+export const ROLES = Object.freeze({
+  admin: { key: "admin" },
+  mentor: { key: "mentor" },
+  coach: { key: "coach" },
+  mentee: { key: "mentee" },
+  alumni: { key: "alumni" },
+  guest: { key: "guest" },
+});
---
+++ src/data/statuses.js
@@ -0,0 +1,5 @@
+export const STATUSES = Object.freeze({
+  active: { key: "active", desc: "" },
+  inactive: { key: "inactive", desc: "" },
+  graduated: { key: "graduated", desc: "" },
+});
