2bbeb897b4d6eda8415ae278672bf7283722d5c7
yusufalp <alpyusuf007@gmail.com>
2024-12-15 16:12:22 -0800

    redirect to create profile if not created at the time of sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,9 +3,10 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import { login } from "../store/features/authSlice";
-import { addProfile } from "../store/features/userSlice";
+import { setProfile } from "../store/features/userSlice";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_AUTH_URL = import.meta.env.VITE_AUTH_URL;
+const API_SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -26,7 +27,7 @@ function Login() {
     const body = { username, password };
 
     try {
-      const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+      const loginResponse = await fetch(`${API_AUTH_URL}/auth/login`, {
         method: "POST",
         credentials: "include",
         headers: {
@@ -35,20 +36,38 @@ function Login() {
         body: JSON.stringify(body),
       });
 
-      const result = await response.json();
+      const loginResult = await loginResponse.json();
 
-      if (result.error) {
-        throw new Error(result.error.message);
+      if (loginResult.error) {
+        throw new Error(loginResult.error.message);
       }
 
-      const accessToken = result.data.accessToken;
-      const expiresAt = result.data.expiresAt;
-      const profile = result.data.profile;
+      const { accessToken, expiresAt } = loginResult.data;
 
       dispatch(login({ accessToken, expiresAt }));
-      dispatch(addProfile({ profile }));
 
-      navigate("/dashboard");
+      const profileResponse = await fetch(
+        `${API_SERVER_URL}/api/profiles/profile`,
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const profileResult = await profileResponse.json();
+
+      if (profileResult.error) {
+        navigate("/create-profile");
+      } else {
+        const { profile } = profileResult.data;
+
+        dispatch(setProfile({ profile }));
+        navigate("/dashboard");
+      }
     } catch (error) {
       setError(error.message);
     } finally {
