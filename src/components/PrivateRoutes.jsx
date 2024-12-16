1e88264fd474d6d1bf40f4626031603b5b72fdb4
yusufalp <alpyusuf007@gmail.com>
2024-12-15 16:12:55 -0800

    adjust the logic to redirect user to the correct page, depending on access token and profile

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,19 +1,41 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
+import { PUBLIC_PATHS } from "../constants/paths";
+
 function PrivateRoutes() {
   const accessToken = useSelector((state) => state.auth.accessToken);
+  const profile = useSelector((state) => state.user.profile);
 
   const location = useLocation();
 
-  const isAuthRoute =
-    location.pathname === "/login" || location.pathname === "/signup";
-
+  // Allow unauthenticated users only to public paths
   if (!accessToken) {
-    return isAuthRoute ? <Outlet /> : <Navigate to="/login" replace />;
+    return PUBLIC_PATHS[location.pathname] ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/login" replace />
+    );
   }
 
-  return isAuthRoute ? <Navigate to="/dashboard" replace /> : <Outlet />;
+  // If authenticated but no profile, redirect to create profile
+  if (!profile) {
+    return <Navigate to="/create-profile" replace />;
+  }
+
+  // Prevent logged-in users with a profile from accessing public paths or creating profile page
+  if (profile) {
+    return PUBLIC_PATHS[location.pathname] ? (
+      <Navigate to="/dashboard" replace />
+    ) : location.pathname === "/create-profile" ? (
+      <Navigate to="/profile" replace />
+    ) : (
+      <Outlet />
+    );
+  }
+
+  // Authenticated users with a profile can access all other routes
+  return <Outlet />;
 }
 
 export default PrivateRoutes;
