70f2942e3c7a13cbbb4bddd16ac81e1379eb059b
yusufalp <alpyusuf007@gmail.com>
2025-01-09 20:23:35 -0800

    refactor graduation date output

--- src/components/Profile/learner/LearnerProfile.jsx
+++ src/components/Profile/learner/LearnerProfile.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 
 function LearnerProfile({ cohort, graduationDate }) {
+  const formattedGraduationDate = graduationDate
+    ? new Date(graduationDate).toLocaleDateString()
+    : "will be updated soon";
+
   return (
     <>
-      <p>Cohort {cohort || "will be updated soon"}</p>
-      <p>Graduation date {graduationDate || "will be updated soon"}</p>
+      <p>Cohort: {cohort ?? "will be updated soon"}</p>
+      <p>Graduation date: {formattedGraduationDate}</p>
     </>
   );
 }
