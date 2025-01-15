ad55b0573273ab67ce87d2004d2269215153f343
yusufalp <alpyusuf007@gmail.com>
2025-01-14 21:09:43 -0800

    refactor

--- src/components/advisor/Advisor.jsx
+++ src/components/advisor/Advisor.jsx
@@ -2,39 +2,48 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
-const PROFILE_SERVICE_URL = import.meta.env.VITE_PROFILE_SERVICE_URL;
+import { constructUrl } from "../../utils/url";
 
 function Advisor() {
   const accessToken = useSelector((state) => state.auth.accessToken);
 
   const { advisorId } = useParams();
 
-  const [advisor, setAdvisor] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState("");
 
   useEffect(() => {
     const getAdvisor = async () => {
+      setError(null);
+      setIsLoading(true);
+
+      const baseUrl = import.meta.env.VITE_PROFILE_SERVICE_URL;
+      const endpoint = "/api/admin/profiles/:advisorId";
+      const params = { advisorId };
+      const query = {};
+
+      const url = constructUrl(baseUrl, endpoint, params, query);
+
+      const options = {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
       try {
-        const response = await fetch(
-          `${PROFILE_SERVICE_URL}/api/profiles/${advisorId}`,
-          {
-            method: "GET",
-            credentials: "include",
-            headers: {
-              "Content-type": "application/json",
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
+        const response = await fetch(url, options);
 
         const result = await response.json();
 
-        if (result.error) {
-          throw new Error(result.error.message);
+        if (result.error || !response.ok) {
+          throw new Error(result.error.message || "Failed to get advisor");
         }
 
-        setAdvisor(result.data.profile);
+        setData(result.data);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -47,14 +56,16 @@ function Advisor() {
 
   return (
     <main>
-      {isLoading && <p>Loading...</p>}
-
-      {error && <p>{error}</p>}
-
-      {!isLoading && !error && advisor && (
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : data ? (
         <>
-          <h1>{`You are viewing ${advisor?.name?.first}'s Profile`}</h1>
+          <h1>{`You are viewing ${data.profile?.name?.firstName}'s Profile`}</h1>
         </>
+      ) : (
+        <p>There is no advisor</p>
       )}
     </main>
   );
